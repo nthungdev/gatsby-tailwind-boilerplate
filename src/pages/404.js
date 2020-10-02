12cdb741d4fa6c50c49b7d7ddd67f786a915fcda
Hung Nguyen <nthungdev@gmail.com>
2020-10-02 17:52:13 -0400

    Add 404 Page

---
+++ src/pages/404.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import Layout from "components/layouts/layout"
+import SEO from "components/seo"
+
+export default () => {
+  return (
+    <Layout>
+      <SEO title="Page Not Found" />
+      <div className="text-center">
+        <p className="text-4xl">Oops!</p>
+        <h1>404 - PAGE NOT FOUND</h1>
+        <p>
+          The page you are looking for might have been removed had its name
+          changed or is temporarily unavailable.
+        </p>
+      </div>
+    </Layout>
+  )
+}
