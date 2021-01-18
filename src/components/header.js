ca4b99ece75c83831618016271c14715795ab378
Hung Nguyen <nthungdev@gmail.com>
2021-01-18 16:24:54 -0500

    Change Header background color

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default () => {
   return (
-    <header className="p-4 bg-gray-300">
+    <header className="p-4 bg-gray-100">
       <div className="container mx-auto">{`Header`}</div>
     </header>
   )
